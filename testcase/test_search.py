2e524fdd43bc3c34c0ea8f0244fbc2807bbac94e
wangpingyi <879001454@qq.com>
2022-03-06 23:27:11 +0800

    增加测试报告

--- testcase/test_search.py
+++ testcase/test_search.py
@@ -6,10 +6,17 @@
 
 """
 import sys
+import unittest
+from BeautifulReport import BeautifulReport
+import datetime
+
 sys.path.append('D:\pom')
 from myunit import Munit
 from pages.baidu_page import BaiduPage
 from time import sleep
+
+
+filename = '自动化测试报告'+datetime.datetime.now().strftime('%Y_%m%d_%H%M%S')
 
 class TestCase(Munit):
 
@@ -17,3 +24,9 @@ class TestCase(Munit):
         bd = BaiduPage(self.driver)
         bd.suosou('全国姓谭的人数')
         sleep(3)
+
+
+if __name__ == '__main__':
+    tc = unittest.defaultTestLoader.loadTestsFromTestCase(TestCase)
+    suit = unittest.TestSuite(tc)
+    BeautifulReport(suit).report(description='自动化测试报告',filename=filename,report_dir='./report')
